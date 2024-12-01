25f3291d804b0e07ba50fc705ff9b0da3e661739
Gajanan Kuchgave <gajanankuchgave@gmail.com>
2024-12-01 17:07:32 -0500

    Added code for 95% training model

--- model.py
+++ model.py
@@ -4,24 +4,19 @@ import torch.nn as nn
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(8)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(16)
-        self.fc1 = nn.Linear(16 * 7 * 7, 64)
-        self.fc2 = nn.Linear(64, 10)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.25)
+        self.fc1 = nn.Linear(16 * 7 * 7, 24)
+        self.fc2 = nn.Linear(24, 10)
 
     def forward(self, x):
-        x = self.pool(self.relu(self.bn1(self.conv1(x))))
-        x = self.dropout(x)
-        x = self.pool(self.relu(self.bn2(self.conv2(x))))
-        x = self.dropout(x)
+        x = self.pool(torch.relu(self.bn1(self.conv1(x))))
+        x = self.pool(torch.relu(self.bn2(self.conv2(x))))
         x = x.view(-1, 16 * 7 * 7)
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
+        x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
